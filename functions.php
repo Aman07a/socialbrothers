e60ae669c35bc87f6ad149f84c96980abdd3974d
Aman07a <aman16.aa17@gmail.com>
2023-10-21 12:18:16 +0200

    Added bootstrap-css to the project

--- functions.php
+++ functions.php
@@ -9,3 +9,9 @@ require_once get_template_directory() . "/lib/init.php";
  * @see https://developer.wordpress.org/reference/hooks/use_block_editor_for_post/
  */
 add_filter('use_block_editor_for_post', '__return_false', 10, 0);
+
+function enqueue_bootstrap_css()
+{
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . './dist/bootstrap.min.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css');
