e43eb5df8d680f9eb9b51633d0590d5fa522f4e2
Aman07a <aman16.aa17@gmail.com>
2023-10-23 21:05:42 +0200

    Added Medium Width Banner to Search Page

--- custom-search-results.php
+++ custom-search-results.php
@@ -8,6 +8,18 @@ defined('ABSPATH') || exit('Forbidden');
 get_header();
 ?>
 
+<!-- Medium Width Banner -->
+<div class="sm-w-banner">
+    <div class="image-container">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/215d1c7002ed987517e77ded5f92d0e6489e9f01.jpg'; ?>" alt="Image" class="img-fluid">
+        <div class="search-banner-container">
+            <div class="search-banner-cover">
+                <h2 class="search-banner-title">Zoekresultaten</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
 Search Results
 
 <?php
