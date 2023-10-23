8004c03c113da7102f6e49c17d40f5e9c77bc194
Aman07a <aman16.aa17@gmail.com>
2023-10-23 09:30:30 +0200

    Medium Banner

---
+++ custom-blog-detail.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Custom Blog Detail
+*/
+
+defined('ABSPATH') || exit('Forbidden');
+
+get_header();
+?>
+<!-- Medium Banner -->
+<div class="md-banner">
+    <div class="image-container">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/8d6cb4608c3a0c50624325b0a2b4d0142822345b.jpg'; ?>" alt="Image" class="img-fluid">
+    </div>
+</div>
+
+Blog Detail
+
+<?php
+
+get_footer();
