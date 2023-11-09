e69d0f356a86d887a566a6ac9c618b5d56c8a5b0
Aman07a <aman16.aa17@gmail.com>
2023-11-09 16:09:45 +0100

    Updated Comments from footer.php

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@ wp_footer();
 
 ?>
 
+<!-- Scripts -->
 <script>
     const cards = document.querySelectorAll('.card');
 
@@ -16,7 +17,7 @@ wp_footer();
     });
 </script>
 
-<!-- JavaScript -->
+<!-- Bootstrap: Scripts -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
