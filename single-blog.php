c0de4a76173e31be97b14f71e6bc676b05c22198
Aman07a <aman16.aa17@gmail.com>
2023-11-11 21:26:20 +0100

    Added Dynamic to Single Blog

--- single-blog.php
+++ single-blog.php
@@ -73,85 +73,110 @@ get_header();
         </h3>
 
         <div class="row card-container row-gap">
-            <div class="col-xl-4 col-lg-6 auto">
-                <div class="card auto">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/images/9a4c5e7ca7beb10f9911b04ae5a9019ca806488d.jpg'; ?>" class="card-img-top" alt="">
-                    <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                        <form class="card__type-form card__type-interview">
-                            <button class="card__type-button" disabled>
-                                interview
-                            </button>
-                        </form>
-                    </div>
-                    <p class="card__title">
-                        Werken bij Social Brothers, <br />
-                        volgens developer Bas
-                    </p>
-                    <p class="card__text">
-                        Lorem ipsum dolor sit amet, consectetur <br />
-                        adipiscing elit. Praesent luctus velit id ex <br />
-                        vestibulum, in tristique risus tincidunt.
-                    </p>
-                    <form action="#blog" class="card__form">
-                        <button disabled>
-                            Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                        </button>
-                    </form>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-6 auto">
-                <div class="card auto">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/images/53c8722145b6fd9e358adfe7b3d05479c10aa9d5.jpg'; ?>" class="card-img-top" alt="">
-                    <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                        <form class="card__type-form card__type-interview">
-                            <button class="card__type-button" disabled>
-                                interview
-                            </button>
-                        </form>
-                    </div>
-                    <p class="card__title">
-                        Werken bij Social Brothers, <br />
-                        volgens developer Lisa
-                    </p>
-                    <p class="card__text">
-                        Lorem ipsum dolor sit amet, consectetur <br />
-                        adipiscing elit. Praesent luctus velit id ex <br />
-                        vestibulum, in tristique risus tincidunt.
-                    </p>
-                    <form action="#blog" class="card__form">
-                        <button disabled>
-                            Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                        </button>
-                    </form>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-6 auto">
-                <div class="card auto">
-                    <img src="<?php echo get_template_directory_uri() . '/assets/images/ed25aee744d9f6b5be4295767984a2151c31fb7f.jpg'; ?>" class="card-img-top" alt="">
-                    <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                        <form class="card__type-form card__type-interview">
-                            <button class="card__type-button" disabled>
-                                interview
-                            </button>
-                        </form>
-                    </div>
-                    <p class="card__title">
-                        Werken bij Social Brothers, <br />
-                        volgens developer Wendy
-                    </p>
-                    <p class="card__text">
-                        Lorem ipsum dolor sit amet, consectetur <br />
-                        adipiscing elit. Praesent luctus velit id ex <br />
-                        vestibulum, in tristique risus tincidunt.
-                    </p>
-                    <form action="#blog" class="card__form">
-                        <button disabled>
-                            Lees meer
-                            <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                        </button>
-                    </form>
-                </div>
-            </div>
+            <?php
+            // Get current page
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+            // WP_Query for Blogs with category filter and order
+            $blog_args = [
+                'post_type' => 'blog',
+                'posts_per_page' => 3,
+                'orderby' => 'category_name',
+                'order' => 'DESC',
+                'paged' => $paged,
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'category',
+                        'field' => 'slug',
+                        'terms' => 'interview',
+                    ),
+                ),
+            ];
+
+            $blog_query = new WP_Query($blog_args);
+            if ($blog_query->have_posts()) {
+                // Create an array to store post data
+                $blogs_posts_array = [];
+
+                // Loop through the posts and add data to the array
+                while ($blog_query->have_posts()) {
+                    $blog_query->the_post();
+
+                    // Get the categories for the current post
+                    $categories = get_the_category(get_the_ID());
+
+                    // Initialize an array to store category names
+                    $category_names = [];
+
+                    // Loop through the categories and add names to the array
+                    foreach ($categories as $category) {
+                        $category_names[] = $category->name;
+                    }
+
+                    // Check if the post has any of the specified types
+                    if (has_term('blogs', 'type', get_the_ID())) {
+                        $post_data = [
+                            'ID' => get_the_ID(),
+                            'post_title' => get_the_title(),
+                            'post_content' => get_the_content(),
+                            'post_name' => get_post_field('post_name'),
+                            'type' => wp_get_post_terms(get_the_ID(), 'type'),
+                            'categories' => $category_names,
+                            'featured_image' => get_the_post_thumbnail_url(
+                                get_the_ID(),
+                                'full'
+                            ),
+                        ];
+
+                        $blogs_posts_array[] = $post_data;
+                    }
+                }
+
+                // Reset post data to the main query
+                wp_reset_postdata();
+
+                // Convert the array to JSON
+                $json_blogs_data = json_encode(
+                    $blogs_posts_array,
+                    JSON_PRETTY_PRINT
+                );
+
+                // Decode the JSON data
+                $blogs_posts_data = json_decode($json_blogs_data, true);
+
+                // Check if there are posts
+                if (!empty($blogs_posts_data)) {
+                    // Loop through the posts
+                    foreach ($blogs_posts_data as $post) {
+            ?>
+                        <div class="col-xl-4 col-lg-6 auto">
+                            <div class="card auto">
+                                <img src="<?php echo esc_html($post['featured_image']); ?>" class="card-img-top card__img--cover" alt="">
+                                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
+                                    <form class="card__type-form card__type-<?php echo esc_html($post['categories'][0]); ?>">
+                                        <button class="card__type-button" disabled>
+                                            <?php echo esc_html($post['categories'][0]); ?>
+                                        </button>
+                                    </form>
+                                </div>
+                                <p class="card__title">
+                                    <?php echo esc_html($post['post_title']); ?>
+                                </p>
+                                <p class="card__text">
+                                    <?php echo esc_html($post['post_content']); ?>
+                                </p>
+                                <form action="<?php echo get_home_url() . '/blog/' . esc_html($post['post_name']); ?>" class="card__form">
+                                    <button>
+                                        Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+            <?php
+                    }
+                }
+            }
+            ?>
         </div>
     </div>
 </div>
