43917ec35b28cc4f7961b669d1ff245c3c9d37b8
Aman07a <aman16.aa17@gmail.com>
2023-11-11 14:29:05 +0100

    Added WP_Query() + Pagination to Events Archive
    (custom-events-archive.php)

--- custom-events-archive.php
+++ custom-events-archive.php
@@ -42,286 +42,117 @@ get_header();
 
     <!-- Events -->
     <div class="row event-container row-gap">
-        <!-- Row 1 -->
+        <?php
+        // Get current page
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+// WP_Query for Events
+$event_args = [
+    'post_type' => 'event',
+    'posts_per_page' => 9,
+    'orderby' => 'date',
+    'order' => 'ASC',
+    'paged' => $paged,
+];
+
+$event_query = new WP_Query($event_args);
+if ($event_query->have_posts()) {
+    // Create an array to store post data
+    $events_posts_array = [];
+
+    // Loop through the posts and add data to the array
+    while ($event_query->have_posts()) {
+        $event_query->the_post();
+
+        // Get the categories for the current post
+        $categories = get_the_category(get_the_ID());
+
+        // Initialize an array to store category names
+        $category_names = [];
+
+        // Loop through the categories and add names to the array
+        foreach ($categories as $category) {
+            $category_names[] = $category->name;
+        }
+
+        // Check if the post has any of the specified types
+        if (has_term('events', 'type', get_the_ID())) {
+            $post_data = [
+                'ID' => get_the_ID(),
+                'post_title' => get_the_title(),
+                'post_content' => get_the_content(),
+                'post_name' => get_post_field('post_name'),
+                'type' => wp_get_post_terms(get_the_ID(), 'type'),
+                'categories' => $category_names,
+                'featured_image' => get_the_post_thumbnail_url(
+                    get_the_ID(),
+                    'full'
+                ),
+            ];
+
+            $events_posts_array[] = $post_data;
+        }
+    }
+
+    // Reset post data to the main query
+    wp_reset_postdata();
+
+    // Convert the array to JSON
+    $json_events_data = json_encode(
+        $events_posts_array,
+        JSON_PRETTY_PRINT
+    );
+
+    // Decode the JSON data
+    $events_posts_data = json_decode($json_events_data, true);
+
+    // Check if there are posts
+    if (!empty($events_posts_data)) {
+        // Loop through the posts
+        foreach ($events_posts_data as $post) {
+            ?>
         <div class="col-xl-4 col-lg-6 auto">
             <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/a921bc847707d8528e4f5e00a324136d69807ae9.jpg'; ?>" class="card-img-top" alt="">
+                <img src="<?php echo esc_html($post['featured_image']); ?>" class="card-img-top card__img--cover"
+                    alt="">
                 <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
+                    <form class="card__type-form card__type-<?php echo esc_html($post['categories'][0]); ?>">
                         <button class="card__type-button" disabled>
-                            utrecht
+                            <?php echo esc_html($post['categories'][0]); ?>
                         </button>
                     </form>
                 </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
                 <p class="card__title">
-                    Het jaarlijkse bierpongtournooi van <br />
-                    Social Brothers
+                    <?php echo esc_html($post['post_title']); ?>
                 </p>
                 <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
+                    <?php echo esc_html($post['post_content']); ?>
                 </p>
-                <form action="/event1" class="card__form">
+                <form action="<?php echo get_home_url().'/event/'.esc_html($post['post_name']); ?>" class="card__form">
                     <button>
                         Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
                     </button>
                 </form>
             </div>
         </div>
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/09430a3f35a25fe2ba038840a7d4b99ba6e39942.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Bootcamp op de donderdagavond <br />
-                    met Biku
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#cards" class="card__form">
-                    <button disabled>
-                        Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/7bf8b7439accaae1dbcb6690298ed5dd6b196eb0.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Blijf up to date met de <br />
-                    maandafsluiting
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#cards" class="card__form">
-                    <button disabled>
-                        Lees meer
-                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-
-        <!-- Row 2 -->
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/09430a3f35a25fe2ba038840a7d4b99ba6e39942.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Bootcamp op de donderdagavond <br />
-                    met Biku
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#cards" class="card__form">
-                    <button disabled>
-                        Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/7bf8b7439accaae1dbcb6690298ed5dd6b196eb0.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Blijf up to date met de <br />
-                    maandafsluiting
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#cards" class="card__form">
-                    <button disabled>
-                        Lees meer
-                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/a921bc847707d8528e4f5e00a324136d69807ae9.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Het jaarlijkse bierpongtournooi van <br />
-                    Social Brothers
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#event" class="card__form">
-                    <button disabled>
-                        Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-
-        <!-- Row 3 -->
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/7bf8b7439accaae1dbcb6690298ed5dd6b196eb0.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Blijf up to date met de <br />
-                    maandafsluiting
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#event" class="card__form">
-                    <button disabled>
-                        Lees meer
-                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/a921bc847707d8528e4f5e00a324136d69807ae9.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Het jaarlijkse bierpongtournooi van <br />
-                    Social Brothers
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#event" class="card__form">
-                    <button disabled>
-                        Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-6 auto">
-            <div class="card auto">
-                <img src="<?php echo get_template_directory_uri().'/assets/images/09430a3f35a25fe2ba038840a7d4b99ba6e39942.jpg'; ?>" class="card-img-top" alt="">
-                <div class="card-img-overlay card__overlay d-flex flex-column justify-content-end">
-                    <form class="card__type-form card__type-interview">
-                        <button class="card__type-button" disabled>
-                            utrecht
-                        </button>
-                    </form>
-                </div>
-                <div class="event__date">
-                    01-01-2023
-                </div>
-                <p class="card__title">
-                    Bootcamp op de donderdagavond <br />
-                    met Biku
-                </p>
-                <p class="card__text">
-                    Lorem ipsum dolor sit amet, consectetur <br />
-                    adipiscing elit. Praesent luctus velit id ex <br />
-                    vestibulum, in tristique risus tincidunt.
-                </p>
-                <form action="#event" class="card__form">
-                    <button disabled>
-                        Lees meer <i class="fa-sharp fa-solid fa-arrow-right"></i>
-                    </button>
-                </form>
-            </div>
-        </div>
+        <?php
+        }
+    }
+}
+?>
     </div>
 
+    <!-- Pagination outside the loop and card container -->
     <div class="pagination">
         <div class="pagination-container">
-            <i class="fa-sharp fa-solid fa-chevron-left"></i>
-            <div class="pagination__block">1</div>
-            <div class="pagination__block pagination__block--active">2</div>
-            <div class="pagination__block">3</div>
-            <div class="pagination__block">...</div>
-            <div class="pagination__block">7</div>
-            <i class="fa-sharp fa-solid fa-chevron-right"></i>
+            <?php
+                echo paginate_links([
+                    'total' => $event_query->max_num_pages,
+                    'current' => max(1, get_query_var('paged')),
+                    'prev_text' => '<i class="fa-sharp fa-solid fa-chevron-left"></i>',
+                    'next_text' => '<i class="fa-sharp fa-solid fa-chevron-right"></i>',
+                ]);
+?>
         </div>
     </div>
 </div>
