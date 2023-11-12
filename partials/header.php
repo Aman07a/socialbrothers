dad85ead77c0d49c30560225d2122363a8f8334a
Aman07a <aman16.aa17@gmail.com>
2023-11-12 13:28:00 +0100

    Added wp_nav_menu() to Header
    (partials/header.php)

--- partials/header.php
+++ partials/header.php
@@ -2,6 +2,79 @@
 
 <header class="header">
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+        <img class="navigation__logo" src="https://socialbrothers.nl/wp-content/uploads/2021/11/sb-logo.svg" alt="Logo">
+      </a>
+      <div class="d-flex navigation-items ml-auto" id="navbarText">
+        <?php
+        wp_nav_menu(array(
+          'menu' => 'top-menu',
+          'menu_id' => 'navigation',
+          'menu_class' => 'navbar-nav',
+          'container' => '',
+          'li_class' => 'nav-item',
+          'active_class' => 'active',
+          'a_class' => 'nav-link',
+        ));
+        ?>
+
+        <!-- Bootstrap: Modal -->
+        <div class="d-flex me-4">
+          <div class="navigation__search">
+            <button type="button" class="modal-button border-0" id="modal-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+        </div>
+
+        <!-- Only open modal on the home page; if not, will be redirected to the home page -->
+        <?php if (!is_front_page()) { ?>
+          <script>
+            const modalButton = document.getElementById("modal-button");
+            if (modalButton) {
+              modalButton.addEventListener("click", function(event) {
+                if (!isFrontPage()) {
+                  window.location.href = home_url("/");
+                }
+              });
+            }
+
+            function isFrontPage() {
+              return window.location.pathname === home_url("/");
+            }
+            if (!isFrontPage()) {
+              modalButton.removeAttribute("data-bs-toggle");
+              modalButton.removeAttribute("data-bs-target");
+            }
+          </script>
+        <?php } ?>
+
+        <!-- Validating if the user is already logged in -->
+        <?php if (is_user_logged_in()) { ?>
+          <div class="me-3">
+            <a href="<?php echo wp_logout_url(home_url('/')); ?>" class="navigation__login">
+              <div class="navigation__login__border">
+                Logout
+              </div>
+            </a>
+          </div>
+        <?php } else { ?>
+          <div class="me-3">
+            <a href="<?php echo esc_url(home_url('/login')); ?>" class="navigation__login">
+              <div class="navigation__login__border">
+                Login
+              </div>
+            </a>
+          </div>
+        <?php } ?>
+      </div>
+    </nav>
+  </div>
+</header>
+
+<header class="header">
+  <div class="container">
     <div class="row header__row">
       <div class="col header__col">
         <a href="/">
@@ -24,28 +97,6 @@
           <i class="fa-solid fa-magnifying-glass"></i>
         </button>
       </div>
-      <?php if (!is_front_page()) { ?>
-        <script>
-          const modalButton = document.getElementById("modal-button");
-
-          if (modalButton) {
-            modalButton.addEventListener("click", function(event) {
-              if (!isFrontPage()) {
-                window.location.href = '/';
-              }
-            });
-          }
-
-          function isFrontPage() {
-            return window.location.pathname === "/";
-          }
-
-          if (!isFrontPage()) {
-            modalButton.removeAttribute("data-bs-toggle");
-            modalButton.removeAttribute("data-bs-target");
-          }
-        </script>
-      <?php } ?>
       <?php if (is_user_logged_in()) { ?>
         <a href="<?php echo wp_logout_url(home_url('/')); ?>" class="col header__col header__border me-4">
           <div class="header__border__login">
@@ -63,6 +114,7 @@
   </div>
 </header>
 
+<!-- Bootstrap: Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content popup border-0">
