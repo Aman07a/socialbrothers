deda5fa342f5791703c84bed5bdc2316a367048a
Aman07a <aman16.aa17@gmail.com>
2023-11-12 18:49:30 +0100

    Removed Old Navigation
    {header.php}

--- partials/header.php
+++ partials/header.php
@@ -67,47 +67,6 @@
   </div>
 </header>
 
-<header class="header">
-  <div class="container">
-    <div class="row header__row">
-      <div class="col header__col">
-        <a href="/">
-          <img class="header__logo" src="https://socialbrothers.nl/wp-content/uploads/2021/11/sb-logo.svg" alt="Logo">
-        </a>
-      </div>
-      <div class="col header__col"></div>
-      <div class="col header__col"></div>
-      <div class="col header__col"></div>
-      <div class="col header__col"></div>
-      <div class="col header__col"></div>
-      <a href="/archive-blog" class="col header__col header__blog">
-        Blog
-      </a>
-      <a href="/archive-events" class="col header__col header__events">
-        Events
-      </a>
-      <div class="col header__col header__search">
-        <button type="button" class="modal-button border-0" id="modal-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          <i class="fa-solid fa-magnifying-glass"></i>
-        </button>
-      </div>
-      <?php if (is_user_logged_in()) { ?>
-        <a href="<?php echo wp_logout_url(home_url('/')); ?>" class="col header__col header__border me-4">
-          <div class="header__border__login">
-            Logout
-          </div>
-        </a>
-      <?php } else { ?>
-        <a href="login" class="col header__col header__border me-4">
-          <div class="header__border__login">
-            Login
-          </div>
-        </a>
-      <?php } ?>
-    </div>
-  </div>
-</header>
-
 <!-- Bootstrap: Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
