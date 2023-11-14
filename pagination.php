7b5b3dd0537922ec707bf5cf04887f21091ff411
Aman07a <aman16.aa17@gmail.com>
2023-11-14 11:33:32 +0100

    Added BEM to Pagination

--- pagination.php
+++ pagination.php
@@ -1,21 +1,21 @@
  <?php
     // Get the post ID from the URL
-    $current_page_id = url_to_postid(get_permalink());
+    $current__page__id = url_to_postid(get_permalink());
 
     // Get the post object
-    $current_post = get_post($current_page_id);
+    $current__post = get_post($current__page__id);
 
     // Get the post slug
-    if ($current_post) {
-        $current_page_slug = $current_post->post_name;
+    if ($current__post) {
+        $current__page__slug = $current__post->post_name;
     } else {
-        $current_page_slug = $current_page_id;
+        $current__page__slug = $current_page_id;
     }
 
     $default__category__event = isset($_GET['events']) ? sanitize_text_field($_GET['events']) : 'utrecht';
     ?>
 
- <form action="<?php echo esc_url(home_url($current_page_slug)); ?>" method="get" class="filter">
+ <form action="<?php echo esc_url(home_url($current__page__slug)); ?>" method="get" class="filter">
      <div class="event-filter__form <?php echo ($default__category__event === 'utrecht') ? 'event-filter--selected' : ''; ?>">
          <button class="event-filter__button <?php echo ($default__category__event === 'utrecht') ? 'event-button--selected' : ''; ?>" name="events" value="utrecht">
              events utrecht
